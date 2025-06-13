a1bd357f4703c72d562935cda6b89f412e551314
Daniel Garcia <danigarciaberzal@gmail.com>
2025-06-13 20:04:29 +0200

    refactor: funcion getEmployeesData

--- src/core/birthday/services/BirthdayService.ts
+++ src/core/birthday/services/BirthdayService.ts
@@ -13,24 +13,10 @@ export class BirthdayService {
     smtpHost: string,
     smtpPort: number
   ) {
-    const data = fs.readFileSync(
-      path.resolve(__dirname, `../../../../resources/${fileName}`),
-      "UTF-8"
-    );
+    const employees = getEmployeesData(fileName);
 
-    // split the contents by new line
-    const lines = data.split(/\r?\n/);
-    lines.shift();
-
-    // print all lines
-    lines.forEach((line) => {
-      const employeeData = line.split(", ");
-      const employee = new Employee(
-        employeeData[1],
-        employeeData[0],
-        employeeData[2],
-        employeeData[3]
-      );
+    //enviar correos
+    employees.forEach((employee) => {
       if (employee.isBirthday(ourDate)) {
         const recipient = employee.getEmail();
         const body = "Happy Birthday, dear %NAME%!".replace(
@@ -79,3 +65,26 @@ export class BirthdayService {
 }
 
 export interface Message extends SMTPTransport.Options, Mail.Options {}
+function getEmployeesData(fileName: string): Employee[] {
+  const data = fs.readFileSync(
+    path.resolve(__dirname, `../../../../resources/${fileName}`),
+    "UTF-8"
+  );
+
+  // split the contents by new line
+  const lines = data.split(/\r?\n/);
+  lines.shift();
+
+  // get employees
+  const employees = lines.map((line) => {
+    const employeeData = line.split(", ");
+    return new Employee(
+      employeeData[1],
+      employeeData[0],
+      employeeData[2],
+      employeeData[3]
+    );
+  });
+  return employees;
+}
+
